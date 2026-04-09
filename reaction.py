75a56d9d6a702acfccd8d70978c5da86b3faeed4
hyfgg <hyfgg20070723@qq.com>
2026-04-09 14:53:15 +0800

    Step 3: Button Detection Function

--- reaction.py
+++ reaction.py
@@ -3,6 +3,15 @@ from time import sleep
 from random import uniform
 
 led = LED(4)
+left_button = Button(14)
+right_button = Button(15)
+
 led.on()
-sleep(uniform(5,10))
+sleep(uniform(5, 10))
 led.off()
+
+def pressed(button):
+    print(str(button.pin.number) + ' won the game')
+
+right_button.when_pressed = pressed
+left_button.when_pressed = pressed

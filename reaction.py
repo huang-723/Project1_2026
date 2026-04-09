a3587d79ceeb275a98a59056da3b427cb2a8942b
lmx <3057163193@qq.com>
2026-04-09 15:10:16 +0800

    步骤3：按钮检测功能

--- reaction.py
+++ reaction.py
@@ -2,8 +2,20 @@ from gpiozero import LED, Button
 from time import sleep
 from random import uniform
 
+# 硬件初始化
 led = LED(4)
+left_button = Button(14)
+right_button = Button(15)
+
+# 灯光逻辑
 led.on()
-# 随机延时5-10秒
 sleep(uniform(5, 10))
 led.off()
+
+# 按钮按压函数
+def pressed(button):
+    print(str(button.pin.number) + ' won the game')
+
+# 绑定按压事件
+right_button.when_pressed = pressed
+left_button.when_pressed = pressed
